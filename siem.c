12923d1b0cee57f62e4e3eece5262c8e8db9d1ba
NoahBarcomb <32barcomb32@gmail.com>
2024-12-23 00:38:31 -0500

    Network Functionality and Skeleton Code

--- siem.c
+++ siem.c
@@ -3,7 +3,64 @@
 
 
 int main() {
+
+    WSADATA wsaData;
+
+    int result = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (result != 0) {
+        printf("WSA Startup Failed: %d\n", result);
+        exit(1);
+    }
+
+    struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    /* reuse result variable */
+    result = getaddrinfo(NULL, PORT, &hints, &result);
+    if (result != 0) {
+        printf("getaddrinfo failed: %d\n", result);
+        WSACleanup();
+        exit(1);
+    }
+
+    /* create server socket */
+
+    SOCKET serverSock = INVALID_SOCKET;
+
+    serverSock = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+    /* make sure socket is valid */
+
+    if (serverSock == INVALID_SOCKET) {
+        printf("socket() error: %ld\n", WSAetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        exit(1);
+    }
+
+
+    /* bind socket */
+
+    result = bind(serverSock, result->ai_addr, (int)result->ai_addrlen);
+
+    /* make sure bind call completed successfully */
+    if (result == SOCKET_ERROR) {
+        printf("bind() error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(serverSock);
+        WSACleanup();
+        exit(1);
+    }
+
+    /* no longer needed */
+    freeaddrinfo(result);
+    
     while (1) {
-        
+
     }
 }
--- siem.h
+++ siem.h
@@ -1,5 +1,13 @@
+#define WIN32_LEAN_AND_MEAN
+#define PORT "20000" /* port for server to run on */
+
+#include <stdlib.h> /* std functions */
 #include <stdio.h> /* printing functionality */
 #include <winsock2.h> /* server */
+#include <windows.h>
+
+// link with ws2_32.lib for network functionality
+#pragma comment (lib, "Ws2_32.lib")
 
 struct User {
     /* represents a user */
@@ -26,7 +34,7 @@ struct Event {
 
 
 /* logs an event to the database */
-void logEvent();
+void logEvent(struct* Event);
 
 /* fetches report of the database */
 void fetchReport(int reportType);
